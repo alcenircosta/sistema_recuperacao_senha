d5174ef028b985b17abb19921a728b29cd09eb6f
Alcenir Costa <alcenir.1994@gmail.com>
2020-11-03 00:01:18 -0200

    Created recuperar.php

---
+++ pages/recuperar.php
@@ -0,0 +1,8 @@
+<form method="post">
+    <h4>Recuperar Senha</h4>
+    <label>Insira o email Cadastrado</label>
+    <input type="text" name="email" class="form-control" />
+    <code>Insira o email cadastrado para receber orientações de redefinição de senha por email!</code><br><br><br>
+    <input type="submit" value="Recuperar" class="btn btn-outline-success btn-lg btn-block">
+    <input type="hidden" name="env" value="form" />
+</form>
