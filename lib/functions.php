2f5550f38afc82dd0f234d034db2376949a3d1de
Alcenir Costa <alcenir.1994@gmail.com>
2020-11-03 00:14:49 -0200

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -1,16 +1,14 @@
 <?php
-class LoadPage{
-    public $path;
-    public $dir = 'pages/';
-    function __construct($url,$ext){
-        $this->path = $this->dir.$url.$ext;
-    }
-public function load(){
-      if(file_exists($this->path)){
+
+function loadPages($url, $ext){
+    $dir = 'pages/';
+    $path = $dir.$url.$ext;
+      
+    if(file_exists($this->path)){
         include ($this->path);
       }else{
         echo "<div class='alert alert-danger'>Página não encontrada!</div>";         
       }
     }
-}
+
     ?>
