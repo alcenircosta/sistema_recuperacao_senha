08875e564ca99c0cd673f2bc4da669e8ae24aba9
Alcenir Costa <alcenir.1994@gmail.com>
2020-11-02 22:34:15 -0200

    Initialized bootstrap and created files on lib folder

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt">
+  <head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <title>Recuperar Senha</title>
+  </head>
+  <body>
+    <h1>Recuperar Senha</h1>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" ></script>
+
+  </body>
+</html>
